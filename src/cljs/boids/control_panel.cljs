a9202803ca20aaff1672d4181740ab816c40e008
Luke VanderHart <luke@vanderhart.net>
2013-03-15 15:46:35 -0400

    Add knobs

--- src/cljs/boids/control_panel.cljs
+++ src/cljs/boids/control_panel.cljs
@@ -1,28 +1,80 @@
 (ns boids.control-panel)
 
+(defn map->obj
+  "Given a Clojure map, return a JavaScript object."
+  [m]
+  (reduce (fn [o [k v]]
+            (aset o (name k) v)
+            o)
+          (js-obj) m))
+
 (defn update-fps
-  [div frame-count]
-  (set! (.-innerHTML div) @frame-count)
+  [frame-count]
+  (-> (js/jQuery "#fps")
+      (.html @frame-count))
   (reset! frame-count 0))
 
 (defn init-fps-monitor
   "Create and maintain a FPS indicator that counts how often the the
-  boids atom updates."
-  [boids]
-  (let [frame-count (atom 0)
-        div (.createElement js/document "div")
-        style (.-style div)]
-    (set! (.-color style) "white")
-    (set! (.-backgroundColor style) "black")
-    (set! (.-zIndex style) 1)
-    (set! (.-position style) "absolute")
-    (set! (.-top style) 0)
-    (set! (.-left style) 0)
-    (.appendChild (.-body js/document) div)
-    (add-watch boids :framecounter #(swap! frame-count inc))
-    (.setInterval js/window #(update-fps div frame-count) 1000)))
+  flock atom updates."
+  [flock-atom]
+  (let [frame-count (atom 0)]
+    (-> (js/jQuery "<div>FPS: <span id='fps'></span>")
+        (.css "font-weight" "bold")
+        (.css "margin-bottom" 10)
+        (.appendTo "#control-panel"))
+    (add-watch flock-atom :framecounter #(swap! frame-count inc))
+    (.setInterval js/window #(update-fps frame-count) 1000)))
+
+
+(defn update-label
+  "Updates a label element with the current value of the key in the
+   options atom"
+  [label options-atom key]
+  (.html label (str (name key) ": " (@options-atom key))))
+
+(defn init-control
+  "Create control slider"
+  [options-atom key slider-opts]
+  (let [div (js/jQuery "<div/>")
+        label (js/jQuery "<span/>")
+        update (fn [new-value]
+                 (swap! options-atom assoc key new-value)
+                 (update-label label options-atom key))]
+    (update-label label options-atom key)
+    (.append div label)
+    (.append div (-> (js/jQuery "<div/>")
+                   (.css "font-size" "0.5em")
+                   (.css "margin" 10)
+                   (.slider (map->obj slider-opts))
+                   (.slider "option" "value" (@options-atom key))
+                   (.on "slidechange" (fn [_ ui] (update (.-value ui))))))
+    (.appendTo div "#control-panel")))
+
+(defn init-controls
+  "Create control sliders"
+  [options-atom]
+  (init-control options-atom :max-speed {:min 0 :max 20})
+  (init-control options-atom :steer-force {:min 0 :max 1 :step 0.02})
+  (init-control options-atom :cohere-distance {:min 0 :max 1000})
+  (init-control options-atom :avoid-distance {:min 0 :max 500})
+  (init-control options-atom :align-distance {:min 0 :max 1000}))
+
+(defn create-cp
+  "Creates and returns a jQuery element for the control panel"
+  []
+  (-> (js/jQuery "<div id='control-panel'/>")
+      (.css "z-index" 1)
+      (.css "position" "absolute")
+      (.css "top" 0)
+      (.css "left" 0)
+      (.css "width" 200)
+      (.appendTo "body"))
+)
 
 (defn init
   "Initialize the control panel"
   [options-atom flock-atom]
-  (init-fps-monitor flock-atom))
+  (create-cp)
+  (init-fps-monitor flock-atom)
+  (init-controls options-atom))
