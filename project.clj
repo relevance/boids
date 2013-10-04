e44f9915a7ea40800c6b9b6f4742265b7e7c1fe2
Stuart Sierra <mail@stuartsierra.com>
2013-10-04 15:35:47 -0400

    Update to latest ClojureScript (0.0-1909) and lein-cljsbuild

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject boids "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1909"]
                  [ring/ring-jetty-adapter "1.1.1"]
                  [compojure "1.1.5"]]
   :source-paths ["src/clj"]
   :main boids.server
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[lein-cljsbuild "0.3.3"]]
   :cljsbuild {:crossovers [boids.euclidean-vector boids.behaviors]
               :builds [{:source-paths ["src/cljs"]
                         :compiler {:optimizations :none
